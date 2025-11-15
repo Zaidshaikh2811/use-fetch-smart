f833bb8a195eabfcafe7984fa9ada7ebb7f9c960
Mohammad zaid Shaikh <109480030+Zaidshaikh2811@users.noreply.github.com>
2025-11-15 10:22:41 +0530

    Update package.json description and enhance smartAxios token handling

--- src/smartAxios.ts
+++ src/smartAxios.ts
@@ -37,7 +37,7 @@ export const createSmartAxios = (
             if (!config) return Promise.reject(error);
 
             // ---- Auto Refresh Token ----
-            if (error.response?.status === 403 && refreshTokenFn) {
+            if (error.response?.status === 401 && refreshTokenFn) {
                 console.log("Auto Refresh Token ");
 
                 if (!refreshPromise) refreshPromise = refreshTokenFn();
